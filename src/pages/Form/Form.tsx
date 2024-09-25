1bb58b0fcf4764a327ad1694c47e968c4a200f2a
William Oliveira Silva <williamsilva20062005@gmail.com>
2024-09-24 22:39:25 -0300

    wip(Form): input state

--- src/pages/Form/Form.tsx
+++ src/pages/Form/Form.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { Box, Button, Typography } from "@mui/material";
 import Footer from "../../components/Footer";
 import jpg from "../../assets/jpg.svg";
+import Dots from "./components/Dot";
 import mp3 from "../../assets/mp3.svg";
 import mp4 from "../../assets/mp4.svg";
 import pdf from "../../assets/pdf.svg";
@@ -16,9 +17,14 @@ export default function Form() {
     setFile(selectedFile);
   };
 
+  const [trigger, setTrigger] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  console.log(loading);
+
   return (
     <>
-      <div className="flex flex-col justify-center items-center w-full h-full p-6 relative z-[2]   ">
+      <div className="flex flex-col justify-center items-center w-full h-full p-6 relative z-[2]  gap-2  ">
         <div className="flex flex-col items-center w-full h-fit justify-center  ">
           <h1
             className="font-medium font-DM leading-none text-9xl text-transparent bg-clip-text animate-gradient"
@@ -30,6 +36,8 @@ export default function Form() {
           >
             Giveme
           </h1>
+          <button onClick={() => setLoading((prev) => !prev)}> oii</button>
+          <button onClick={() => setTrigger((prev) => !prev)}> oii2</button>
           <h1
             className="font-normal leading-tight  font-Sora text-[1.72rem] text-transparent bg-clip-text animate-gradient"
             style={{
@@ -42,7 +50,16 @@ export default function Form() {
           </h1>
         </div>
         <div className="w-full h-full flex justify-center items-center ">
-          <Box sx={{ width: "auto", margin: "0 auto", textAlign: "center" }}>
+          <Box
+            sx={{
+              width: "auto",
+              margin: "0 auto",
+              display: "flex",
+              justifyContent: "center",
+              gap: "2rem",
+              flexDirection: "column",
+            }}
+          >
             {/* <Typography variant="h6" sx={{ marginBottom: "10px" }}>
             Upload de Arquivo
           </Typography> */}
@@ -52,40 +69,68 @@ export default function Form() {
             <FileUpload onFileChange={handleFileChange} />
 
             {/* Exibição de nome e tamanho do arquivo */}
-            {file && (
-              <Box className="boxFile">
-                <div className="flex gap-5 items-center">
-                  <div className="w-8 h-8">
-                    <img src={jpg} className="w-full object-contain" />
+
+            <div
+              className={`relative overflow-hidden transition-all duration-300 ease-out ${
+                !file ? `h-0` : `h-[80px]`
+              } `}
+            >
+              <Box className="boxFile h-full relative border-[1px] shadow border-white flex justify-center items-center">
+                {loading ? (
+                  <>
+                    <div className="flex gap-5 items-center">
+                      <div className="w-8 h-8">
+                        <img src={jpg} className="w-full object-contain" />
+                      </div>
+                      <div className="flex flex-col w-auto h-full justify-center">
+                        <h4 className="text-[14px] font-medium text-[#6d6c75]">
+                          {file?.name}
+                        </h4>
+                        <div className="flex text-xs font-regular justify-end gap-2 text-[#6d6c75]">
+                          <p>Size:</p>
+                          <p className="font-medium">
+                            {file
+                              ? `${(file.size / 1024).toFixed(2)} KB`
+                              : "Tamanho não disponível"}
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      sx={{
+                        backgroundColor: "#6d6af7", // Cor de fundo branca
+                        color: "#ffffff", // Cor do texto preta
+                        borderRadius: "20px",
+                        fontSize: "12px",
+                        boxShadow: "none",
+                        textTransform: "none",
+                        transition: "background-color 0.3s", // Transição de cor ao passar o mouse
+                        border: "1px solid #cccc", // Borda preta
+                        "&:hover": {
+                          backgroundColor: "#4E55FE", // Cor de fundo ao passar o mouse
+                          boxShadow: "none", // Retirar sombra ao passar o mouse
+                        },
+                      }}
+                      onClick={() => setTrigger((prev) => !prev)}
+                    >
+                      Create link
+                    </Button>
+                  </>
+                ) : (
+                  <div
+                    className={` relative transition-all duration-1000 ease-in-out w-full  ${
+                      trigger
+                        ? `-translate-y-6 opacity-0 `
+                        : `opacity-100 translate-y-1`
+                    }`}
+                  >
+                    <Dots />
                   </div>
-                  <div className="flex flex-col w-auto">
-                    <h4 className="text-base font-medium text-[#6d6c75]">
-                      {file.name}
-                    </h4>
-                    <h4 className="text-sm text-end font-regular text-[#6d6c75]">
-                      {(file.size / 1024).toFixed(2)} KB
-                    </h4>
-                  </div>
-                </div>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  sx={{
-                    marginTop: "10px",
-                    backgroundColor: "#FFFFFF", // Cor de fundo branca
-                    color: "#000000", // Cor do texto preta
-                    borderRadius: "8px",
-                    boxShadow: "none",
-                    border: "1px solid #cccc", // Borda preta
-                    "&:hover": {
-                      backgroundColor: "#f0f0f0", // Cor de fundo ao passar o mouse
-                    },
-                  }}
-                >
-                  Enviar
-                </Button>
+                )}
               </Box>
-            )}
+            </div>
           </Box>
         </div>
         <Footer />
---
+++ src/pages/Form/components/Dot.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import styled, { keyframes } from "styled-components";
+
+const bounce = keyframes`
+  0%, 80%, 100% {
+    transform: scale(0);
+  }
+  40% {
+    transform: scale(1);
+  }
+`;
+
+const wave = keyframes`
+  0%, 100% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-10px);
+  }
+`;
+
+const DotsWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Dot = styled.div<{ delay: string }>`
+  width: 12px;
+  height: 12px;
+  margin: 0 5px;
+  background-color: #3498db;
+  border-radius: 50%;
+  animation: ${bounce} 1.4s infinite ease-in-out both,
+    /* Animação de pulsação */ ${wave} 1.4s infinite ease-in-out both; /* Animação de movimento vertical */
+  animation-delay: ${({ delay }) => delay};
+`;
+
+const Dots: React.FC = () => {
+  return (
+    <DotsWrapper>
+      <Dot delay="0s" />
+      <Dot delay="0.2s" />
+      <Dot delay="0.4s" />
+    </DotsWrapper>
+  );
+};
+
+export default Dots;
